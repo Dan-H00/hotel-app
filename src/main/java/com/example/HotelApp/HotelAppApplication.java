4812a9b80f0e840ae3601d6dba39ee0e03466e51
Dan Hotico <dan.hotico@accesa.eu>
2024-05-17 17:06:54 +0300

    test commit

--- src/main/java/com/example/HotelApp/HotelAppApplication.java
+++ src/main/java/com/example/HotelApp/HotelAppApplication.java
@@ -3,6 +3,7 @@ package com.example.HotelApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// test commit
 @SpringBootApplication
 public class HotelAppApplication {
 
